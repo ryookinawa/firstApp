c14802bbb3059a0dae84548733f32c3381855807
Ryo Uehara <ryookinawa@gmail.com>
2015-03-12 00:44:19 +0900

    赤外線コードをDBから取り出し

---
+++ routes/db.js
@@ -0,0 +1,48 @@
+/*
+ * Mongooseでデータを取り出して表示するテスト
+ */
+var mongoose = require('mongoose');
+
+// スキーマ定義
+var IrSchema = new mongoose.Schema({
+  name: String,
+  code: String,
+  group: String
+});
+// モデルとして登録
+var ircode = mongoose.model('IrCode', IrSchema);
+
+// mongodbに接続
+mongoose.connect('mongodb://localhost:27017/test', // memoの部分はデータベース名
+  // コールバックでエラー時の処理が書けるみたい。
+  function(err) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log('connection success!');
+    }
+  }
+);
+
+// findしてコンソールに出力
+module.exports = {
+    findDb : function ( cmd , callback ) {
+	ircode.find({ name : cmd }, function(err, docs) {
+  		if(!err) {
+    		console.log("num of item => " + docs.length)
+		console.log (" db.js => " + docs[0].code);
+		callback (docs[0].code);
+    		//for (var i = 0; i < docs.length; i++ ) {
+      		//	console.log(docs[i]);
+      		//	console.log(docs[i].name);
+      		//	console.log(docs[i].code);
+      		//	console.log(docs[i].group);
+    		//}
+    		mongoose.disconnect()  // mongodbへの接続を切断
+    		//process.exit()         // node.js終了
+  		} else {
+    			console.log("find error")
+  		}
+	});
+    }
+}
