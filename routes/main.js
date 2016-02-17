b0a75e1197ef6abb574c7549c26b2bf1bf081344
ryookinawa <ryookinawa@gmail.com>
2016-02-17 23:50:42 +0900

    test commit

--- routes/main.js
+++ routes/main.js
@@ -14,7 +14,7 @@ module.exports = {
     index : function (req, res) {
       res.console.log('test')
       res.console.log('test2')
-      res.render('index', { title: 'Remote ControllllllGGGGGGGHHHHHHIIII' });
+      res.render('index', { title: 'Remote ControllllllGGGGGGGHHHHHHIIIIJJJ' });
     },
     command : function (req, res) {
         // idにより処理を分岐
