1e0543174b669448ca3a1afee89f278d574271c3
ryookinawa <ryookinawa@gmail.com>
2016-02-23 22:29:57 +0900

    test

--- routes/main.js
+++ routes/main.js
@@ -9,7 +9,7 @@ module.exports = {
     index : function (req, res) {
         res.console.log('test')
         res.console.log('test2')
-        res.render('index', { title: 'Remote Controllll' });
+        res.render('index', { title: 'Remote Controllll1111111' });
     },
 
     // app.jsでcommandを読み込んだ場合の処理
