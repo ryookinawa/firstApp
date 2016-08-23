048f4d5e43a9bbcbec0d5f0bc4ca858b9cca01e5
ryookinawa <ryookinawa@gmail.com>
2016-08-23 21:39:52 +0900

    444

--- routes/main.js
+++ routes/main.js
@@ -9,7 +9,7 @@ module.exports = {
     index : function (req, res) {
         res.console.log('test')
         res.console.log('test2')
-        res.render('index', { title: 'Remote Contro333' });
+        res.render('index', { title: 'Remote Contro444' });
     },
 
     // app.jsでcommandを読み込んだ場合の処理
