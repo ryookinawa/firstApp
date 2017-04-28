526c65cb9fda2195d7ab6369440adeae962042ca
ryookinawa <ryookinawa@gmail.com>
2017-04-28 14:31:16 +0900

    Beautify Contents

--- routes/main.js
+++ routes/main.js
@@ -5,42 +5,46 @@ var router = express.Router();
 var irkit = require('./irkit.js');
 
 module.exports = {
-    // app.jsでindexを読み込んだ場合の処理
-    index : function (req, res) {
-        res.console.log('test')
-        res.console.log('test2')
-        res.render('index', { title: 'Remote Control2' });
-    },
+  // app.jsでindexを読み込んだ場合の処理
+  index: function(req, res) {
+    res.console.log('test')
+    res.console.log('test2')
+    res.render('index', {
+      title: 'Remote Control2'
+    });
+  },
 
-    // app.jsでcommandを読み込んだ場合の処理
-    command : function (req, res) {
-        // idにより処理を分岐
-        // idをAPI的に活用する
-        var cmd = req.param('id');
+  // app.jsでcommandを読み込んだ場合の処理
+  command: function(req, res) {
+    // idにより処理を分岐
+    // idをAPI的に活用する
+    var cmd = req.param('id');
 
-       	//res.console.log('------------Start----------');
-	      var statCode = irkit.sendIr(cmd,function(statCode){
-       	      //console.log('main.js => ------------Second----------');
-		          res.send('StatusCode = ' + statCode);
-       	      //console.log("main.js => " + statCode);
-              //console.log('main.js => ------------3333----------');
-              //console.log("main.js => " + statCode);
-	      });
-       	//console.log('main.js => ------------4444----------');
-    },
+    //res.console.log('------------Start----------');
+    var statCode = irkit.sendIr(cmd, function(statCode) {
+      //console.log('main.js => ------------Second----------');
+      res.send('StatusCode = ' + statCode);
+      //console.log("main.js => " + statCode);
+      //console.log('main.js => ------------3333----------');
+      //console.log("main.js => " + statCode);
+    });
+    //console.log('main.js => ------------4444----------');
+  },
 
-    // app.jsでtestを読み込んだ場合の処理
-    test : function (req, res) {
-        //res.render('register');
-        res.render('test');
-    },
+  // app.jsでtestを読み込んだ場合の処理
+  test: function(req, res) {
+    //res.render('register');
+    res.render('test');
+  },
 
-    // app.jsでregisterを読み込んだ場合の処理
-    register : function (req, res) {
-        res.console.log('register directive');
-	      var data = req.body;
-        res.console.log(data);
-        res.render('register', { title: 'Register Code' });
-	      //res.send(200);
-    }
+  // app.jsでregisterを読み込んだ場合の処理
+  register: function(req, res) {
+    res.console.log('register directive');
+    var data = req.body;
+    res.console.log(data);
+    res.render('register', {
+      title: 'Register Code'
+    });
+    //res.send(200);
+  }
 };
